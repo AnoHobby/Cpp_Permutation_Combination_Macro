6702b591c921c6b0f0162fd8a4a2d84849834c34
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2025-02-22 13:06:55 +0900

    Add files via upload

--- PerCoM.hpp
+++ PerCoM.hpp
@@ -1,46 +1,46 @@
 ﻿#ifndef PERCOM_HPP
 #define PERCOM_HPP
+namespace PerCoM{
+    template <class T>
+    class Operator {
+    protected:
+        T value;
+    public:
+        constexpr Operator(T value) : value(value) {}
+        virtual constexpr T operator->*(T r) const = 0;
+    };
 
-template <class T>
-class Operator {
-protected:
-    T value;
-public:
-    constexpr Operator(T value) : value(value) {}
-    virtual constexpr T operator->*(T r) const = 0;
-};
-
-template <class T>
-class P_Op : public Operator<T> {
-public:
-    using Operator<T>::Operator;
-    constexpr T operator->*(T r) const override {
-        T result = 1;
-        for (T i = 0; i < r; ++i) {
-            result *= this->value - i;
+    template <class T>
+    class P_Op : public Operator<T> {
+    public:
+        using Operator<T>::Operator;
+        constexpr T operator->*(T r) const override {
+            T result = 1;
+            for (T i = 0; i < r; ++i) {
+                result *= this->value - i;
+            }
+            return result;
         }
-        return result;
+    };
+
+    template <class T>
+    class C_Op : public Operator<T> {
+    public:
+        using Operator<T>::Operator;
+        constexpr T operator->*(T r) const override {
+            return P_Op<T>(this->value)->*r / P_Op<T>(r)->*r;
+        }
+    };
+
+    template <template <class> class Op>
+    class Helper {};
+
+    template <template <class> class Op>
+    constexpr auto operator->*(auto n, Helper<Op>) {
+        return Op<decltype(n)>(n);
     }
-};
 
-template <class T>
-class C_Op : public Operator<T> {
-public:
-    using Operator<T>::Operator;
-    constexpr T operator->*(T r) const override {
-        return P_Op<T>(this->value)->*r / P_Op<T>(r)->*r;
-    }
-};
-
-template <template <class> class Op>
-class Helper {};
-
-template <template <class> class Op>
-constexpr auto operator->*(auto n, Helper<Op>) {
-    return Op<decltype(n)>(n);
+#define P ->* PerCoM::Helper<PerCoM::P_Op>() ->*
+#define C ->* PerCoM::Helper<PerCoM::C_Op>() ->*
 }
-
-#define P ->* Helper<P_Op>() ->*
-#define C ->* Helper<C_Op>() ->*
-
 #endif
